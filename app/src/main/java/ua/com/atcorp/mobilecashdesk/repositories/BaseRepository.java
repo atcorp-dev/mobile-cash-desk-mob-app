afac2e17900763146df78176a392b37b6b0b54bd
StashBank <opavlovskyi@gmail.com>
2018-09-28 00:08:30 +0300

    uri

--- app/src/main/java/ua/com/atcorp/mobilecashdesk/repositories/BaseRepository.java
+++ app/src/main/java/ua/com/atcorp/mobilecashdesk/repositories/BaseRepository.java
@@ -17,8 +17,8 @@ public abstract class BaseRepository<T> {
         void response(T response, E error);
     }
 
-    // final static String API_URL = "https://mobile-cash-desk.herokuapp.com/api/";
-    final static String API_URL = "http://10.0.2.2:3000/api/";
+    final static String API_URL = "https://mobile-cash-desk.herokuapp.com/api/";
+    // final static String API_URL = "http://10.0.2.2:3000/api/";
 
     protected static final Interceptor getRewriteCacheControlInterceptor() {
         return chain -> {
